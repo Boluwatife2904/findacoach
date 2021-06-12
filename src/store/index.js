611a1bca7258fed9975a00366abdf9aad70ed5df
Boluwatife2904 <sanvicola2000@gmail.com>
2021-06-12 17:07:37 +0100

    feat: persisting vuex data in localstorage

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,15 @@ import { createStore } from "vuex";
 import CoachesModule from "./modules/coaches/index.js";
 import RequestsModule from "./modules/requests/index";
 import AuthModule from "./modules/auth/index.js";
+import createPersistedState from "vuex-persistedstate";
 
 const store = createStore({
   modules: {
     coaches: CoachesModule,
     requests: RequestsModule,
-    auth: AuthModule
+    auth: AuthModule,
   },
+  plugins: [createPersistedState()],
 });
 
 export default store;
