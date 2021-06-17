e410d24868fa00535aacb1b11a327f04f83f6fb5
Boluwatife2904 <sanvicola2000@gmail.com>
2021-06-17 09:59:18 +0100

    Refactored the login and signup signup into one action to avoid code duplication since it was just the url that differs

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,46 +1,36 @@
 export default {
   async signup(context, payload) {
-    const response = await fetch(
-      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAbnbAIShcRaA8ZHG91yK7yaFxsGVZxSL0`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ ...payload, returnSecureToken: true })
-      }
-    );
-    const responseData = await response.json();
-    if (!response.ok) {
-      const error = new Error(
-        responseData.error.message || "An error occurred trying to sign you up."
-      );
-      throw error;
-    }
-    const { idToken, localId, expiresIn } = responseData;
-    context.commit("setUser", {
-      token: idToken,
-      userId: localId,
-      tokenExpiration: expiresIn
-    });
-    context.commit("setAuthState", true);
+    return context.dispatch("auth", {...payload, mode: "signup"})
   },
   async login(context, payload) {
-    const response = await fetch(
-      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAbnbAIShcRaA8ZHG91yK7yaFxsGVZxSL0`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ ...payload, returnSecureToken: true })
-      }
-    );
+    return context.dispatch("auth", {...payload, mode: "login"})
+  },
+  async auth(context, payload) {
+    const mode = payload.mode;
+    let url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAbnbAIShcRaA8ZHG91yK7yaFxsGVZxSL0`;
+    if (mode === "signup") {
+      url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAbnbAIShcRaA8ZHG91yK7yaFxsGVZxSL0`;
+    }
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true
+      })
+    });
     const responseData = await response.json();
     // Error handling goes in here
     if (!response.ok) {
+      let message = "An error occurred trying to log you in.";
+      if (mode === "signup") {
+        message = "An error occured trying to sign you up"
+      }
       const error = new Error(
-        responseData.error.message || "An error occurred trying to sign you up."
+        responseData.error.message || message
       );
       throw error;
     }
